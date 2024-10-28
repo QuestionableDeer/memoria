0cecbb229a7ceb8a880c6ea278b573bc53a91f93
Briar <supersecretdeer27@gmail.com>
2024-10-27 17:23:37 -0700

    create infrastructure to allow uploading alphanumeric codes and locking them

--- storapp/views.py
+++ storapp/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
+from storapp.forms import CodeUploadForm
+from storapp.models import CodeMemory
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -23,3 +26,28 @@ def registration_page(request):
 @login_required
 def profile(request):
     return render(request, 'profile.html')
+
+@login_required
+def upload_code(request):
+    if request.method == 'POST':
+        # create form instance and populate it with data from request (binding)
+        form = CodeUploadForm(request.POST)
+
+        if form.is_valid():
+            codeMemory = CodeMemory()
+            codeMemory.name = form.cleaned_data['name']
+            codeMemory.code = form.cleaned_data['code']
+            codeMemory.owner = request.user
+            codeMemory.is_paid = False
+
+            codeMemory.save()
+
+            return HttpResponseRedirect(reverse('profile'))
+    else:
+        form = CodeUploadForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'storapp/upload_code.html', context)
