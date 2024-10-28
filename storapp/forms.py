609ad762a6517cd81ed183a9e435532100091564
Briar <supersecretdeer27@gmail.com>
2024-10-27 23:15:45 -0700

    add infrastructure for uploading and viewing images

--- storapp/forms.py
+++ storapp/forms.py
@@ -3,3 +3,7 @@ from django import forms
 class CodeUploadForm(forms.Form):
     name = forms.CharField(help_text='A brief name to help identify this code')
     code = forms.CharField(help_text='The code to be stored')
+
+class ImageUploadForm(forms.Form):
+    name = forms.CharField(help_text='A brief name to help identify this image')
+    img = forms.ImageField(help_text='The image to be stored')
--- storapp/models.py
+++ storapp/models.py
@@ -20,3 +20,21 @@ class CodeMemory(models.Model):
 
     class Meta:
         ordering = ['upload_date']
+
+class ImageMemory(models.Model):
+    name = models.CharField(max_length=256)
+    img = models.ImageField()
+    owner = models.ForeignKey(auth_models.User, on_delete=models.CASCADE)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        """String for representing code object."""
+        return self.name
+
+    def get_absolute_url(self):
+        """Returns url to access a particular comment instance."""
+        return reverse('image-detail', args=[str(self.id)])
+
+    class Meta:
+        ordering = ['upload_date']
